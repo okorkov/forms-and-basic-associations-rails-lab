793e592641a8caa37edc459c4fd65e7fdd5ff27c
okorkov <alex.okorkov@gmail.com>
2021-01-15 15:57:03 -0800

    commit tree f40467cb9773381ca74dcc7d8034e2040bc4848d

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -12,8 +12,9 @@ class SongsController < ApplicationController
   end
 
   def create
+    # raise params.inspect
     @song = Song.new(song_params)
-
+    
     if @song.save
       redirect_to @song
     else
@@ -47,7 +48,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_name, :genre_id, :notes_1, :notes_2)
   end
 end
 
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,38 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+ 
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def genre_id=(name)
+    self.genre = Genre.find_by(name: name)
+  end
+
+  def genre_id
+    self.genre ? self.genre.name : nil
+  end
+
+  def notes_1=(content)
+    self.notes << Note.create(content: content)
+  end
+
+  def notes_1
+    self.notes
+  end
+
+  def notes_2=(content)
+    self.notes << Note.create(content: content)
+  end
+
+  def notes_2
+    self.notes
+  end
+
 end
